cea45af856210aff9543234bc1bef57fb297885a
Shreykumar Patel <spate629@uwo.ca>
2024-08-26 19:16:32 -0400

    signup and sign in edit

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,13 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
+    <div className="flex justify-center py-24">
+
       <SignIn />
+
+
+    </div>
+      
     
   );
 }
